38d15c505d35ab158de080656ec2b5122e539d02
trey03 <adixon1@unc.edu>
2023-02-25 01:05:29 -0500

    2st push

--- app/src/main/java/Manager.java
+++ app/src/main/java/Manager.java
@@ -1,7 +1,48 @@
 import java.util.ArrayList;
 
 public class Manager {
-    private ArrayList<Candidate> allCandidates = new ArrayList<>();
-    private ArrayList<Candidate> sortedCandidates = new ArrayList<>();
-    private Arr
+    private ArrayList<Candidate> allCandidates;
+    private ArrayList<Candidate> sortedCandidates;
+    private ArrayList<Candidate> chosenCandidates;
+    private int[] questionsList;
+    public static final int NUM_QUESTIONS = 0;
+    public Manager(boolean isFirstCounty) {
+
+        allCandidates = new ArrayList<>();
+        sortedCandidates = new ArrayList<>();
+        chosenCandidates = new ArrayList<>();
+
+
+        if (isFirstCounty) {
+            //First data set
+        }
+        else{
+            //Second data set
+        }
+        //Initialize questions
+    }
+
+    public ArrayList<Candidate> getAllCandidates() {
+        return allCandidates;
+    }
+    public ArrayList<Candidate> getSortedCandidates() {
+        return sortedCandidates;
+    }
+    public ArrayList<Candidate> getChosenCandidates() {
+        return chosenCandidates;
+    }
+
+    public void addToChosen(Candidate c) {
+        chosenCandidates.add(0, c);
+    }
+    public void removeFromChosen(Candidate c) {
+        chosenCandidates.remove(c);
+    }
+    public int submitQuiz(int[] values) {
+        for (int i = 0; i < questionsList[NUM_QUESTIONS]; i++) {
+            questionsList[i] = values[i];
+        }
+        return 0;
+    }
+
 }
